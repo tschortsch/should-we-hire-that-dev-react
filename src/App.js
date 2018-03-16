1550d2faaeb9bbb69df5b506e8076b6602a8a169
Juerg Hunziker <juerg.hunziker@gmail.com>
2018-03-16 21:51:26 +0100

    import bootstrap styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './bootstrap/bootstrap.scss'
+import './App.scss';
 
 class App extends Component {
   render() {
