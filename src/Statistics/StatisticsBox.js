e2e1c97e4b691ba74b805aa6bf805b67fbb20c03
Juerg Hunziker <juerg.hunziker@gmail.com>
2018-03-18 17:55:14 +0100

    add countup library

--- src/Statistics/StatisticsBox.js
+++ src/Statistics/StatisticsBox.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import CountUp from 'react-countup';
 
 const StatisticsBox = ({ title, value, additionalValue = '', ranking }) => (
   <div className={`col-md-6 mb-4 ${ ranking > 0 ? 'rank-' + ranking : ''  }`}>
     <h3>{title}</h3>
-    <p className="value">{value}</p>
+    <p className="value"><CountUp start={0} end={value} /></p>
     {additionalValue !== '' ?
       <p>{additionalValue}</p>
       : null
